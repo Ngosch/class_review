19b1c85fc00058349a3fc9e054fa8d2145da4475
Ngosch <carl1312@gmail.com>
2023-09-25 17:54:27 +0900

    C-4

--- customer.py
+++ customer.py
@@ -67,3 +67,26 @@ print(tom.entry_fee())  # 1500
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 print(ieyasu.entry_fee())  # 1200
+
+# 課題C-4のコード（課題C-1 + C-2 + C-3 + C-4）
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def info_csv(self):
+        return f"{self.full_name()},{self.age}"
+
+# テスト
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+print(ken.info_csv())  # "Ken Tanaka,15"
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+print(tom.info_csv())  # "Tom Ford,57"
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+print(ieyasu.info_csv())  # "Ieyasu Tokugawa,73"
