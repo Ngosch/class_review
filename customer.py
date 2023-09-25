37bd644bd682457822c9ddd33eb7bd2ceebfd5b5
Ngosch <carl1312@gmail.com>
2023-09-25 17:48:52 +0900

    C-3

--- customer.py
+++ customer.py
@@ -40,3 +40,30 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 print(ieyasu.full_name())  # "Ieyasu Tokugawa"
 print(ieyasu.age)  # 73
 
+# 課題C-3のコード（課題C-1 + C-2 + C-3）
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def entry_fee(self):
+        if self.age < 20:
+            return 1000
+        elif 20 <= self.age < 65:
+            return 1500
+        else:
+            return 1200
+
+# テスト
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+print(ken.entry_fee())  # 1000
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+print(tom.entry_fee())  # 1500
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+print(ieyasu.entry_fee())  # 1200
